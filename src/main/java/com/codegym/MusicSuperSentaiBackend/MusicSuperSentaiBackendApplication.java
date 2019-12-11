7f670bc4a730a4d7e60a67e6d1888e708bfe4bfa
DoMinhHiep97 <54266147+DoMinhHiep97@users.noreply.github.com>
2019-12-11 13:11:31 +0700

    first commit

---
+++ src/main/java/com/codegym/MusicSuperSentaiBackend/MusicSuperSentaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.MusicSuperSentaiBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicSuperSentaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicSuperSentaiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codegym/MusicSuperSentaiBackend/MusicSuperSentaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.MusicSuperSentaiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicSuperSentaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
