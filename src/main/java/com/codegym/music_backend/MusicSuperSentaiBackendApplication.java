07f7af55b0d8a12bcf3f1fb939b622fbbbb9e866
DoanHieu <doanhieu.bl.hn@gmail.com>
2019-12-13 10:47:00 +0700

    jwt , bao mat

--- src/main/java/com/codegym/music_backend/MusicSuperSentaiBackendApplication.java
+++ src/main/java/com/codegym/music_backend/MusicSuperSentaiBackendApplication.java
@@ -1,4 +1,4 @@
-package com.codegym.MusicSuperSentaiBackend;
+package com.codegym.music_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
