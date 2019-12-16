5469eefdc1e6241a02dadc8043e4771c296ec107
thao9415 <thao.9415@gmail.com>
2019-12-16 10:18:34 +0700

    add function save user

--- src/main/java/com/codegym/music_backend/service/IUserService.java
+++ src/main/java/com/codegym/music_backend/service/IUserService.java
@@ -9,8 +9,13 @@ import java.util.Optional;
 public interface IUserService {
 
     Optional<User> findByUsername(String username);
+
     User findById(Long id);
-Boolean existsByUsername(String username);
-Boolean existsByEmail(String email);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    User save(User user);
 
 }
